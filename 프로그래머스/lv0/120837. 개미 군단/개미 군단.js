512e03252fbbdf839635ef52f3cb8fbc1ca53102
MJ <themrsung@gmail.com>
2022-12-06 22:54:22 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,12 +1,3 @@
 function solution(hp) {
-    let leftover = 0
-    let general_count = Math.floor(hp / 5)
-    leftover = hp % 5
-    
-    let sergeant_count = Math.floor(leftover / 3)
-    leftover = leftover % 3
-    
-    let worker_count = leftover
-    
-    return general_count + sergeant_count + worker_count
+    return Math.floor(hp / 5) + Math.floor(hp % 5 / 3) + Math.floor(hp % 5 % 3)
 }
